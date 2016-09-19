d7ba22866df2f981760a3b28587df9c625e521d7
Dias Taufik Rahman <diastaufik@gmail.com>
2016-09-19 20:01:43 +0700

    Add Delete

--- todoapi.php
+++ todoapi.php
@@ -11,32 +11,51 @@ $conn = new PDO('mysql:host='. $host .';dbname='. $dbname, $dbuser, $dbpass);
 
 $method = strtolower($_SERVER['REQUEST_METHOD']);
 
+switch ($method) {
+	case 'post':
+		if (!isset($_POST['name']) || empty(trim($_POST['name']))) {
+			http_response_code(400);
+			die();
+		}
 
-if ($method == 'post') {
-	if (!isset($_POST['name']) || empty(trim($_POST['name']))) {
-		http_response_code(400);
+		$add = $conn->prepare("INSERT INTO todo (name) values (:name)");
+		$add->execute([
+			'name' => $_POST['name'],
+		]);
+
+		http_response_code(200);
 		die();
-	}
+		break;
 
-	$add = $conn->prepare("INSERT INTO todo (name) values (:name)");
-	$add->execute([
-		'name' => $_POST['name'],
-	]);
+	case 'get':
+		$get = $conn->query("SELECT * from todo");
+		if ($get->rowCount() == 0)
+		{
+			echo json_encode([]);
+			die();
+		}
 
-	http_response_code(200);
-	die();
-
-} else {
-
-	$get = $conn->query("SELECT * from todo");
-	if ($get->rowCount() == 0)
-	{
-		echo json_encode([]);
+		echo json_encode($get->fetchAll(PDO::FETCH_OBJ));
 		die();
-	}
+		break;
 
-	echo json_encode($get->fetchAll(PDO::FETCH_OBJ));
-	die();
+	case 'delete':
+		parse_str(file_get_contents('php://input'), $payload);
+		if (!isset($payload['id']) || empty(trim($payload['id'])))  {
+			http_response_code(400);
+			die();
+		} else {
+			$delete = $conn->prepare('DELETE FROM todo WHERE id = :id');
+			$delete->execute([
+				'id' => $payload[id],
+			]);
+
+			http_response_code(200);
+			die();
+		}
+
+		break;
+	default:
+		http_response_code(405);
+		break;
 }
-
-?>
